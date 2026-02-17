034e34e767d709e58cbf5d66af71fd82e949858b
said aziz <aziwazaziz2003@gmail.com>
2026-02-17 21:48:39 +0100

    Fix Vercel deployment - remove secrets

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -3,6 +3,12 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: { port: 5174 },
+   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false
+  },
+  server: {
+    port: 5173
+  }
 });
